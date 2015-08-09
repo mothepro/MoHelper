5eb62b184549e7fe7911dfb8d74439550dcf349f
Maurice Prosper <maurice.prosper@ttu.edu>
2015-08-09 12:54:50 -0500

    More helpful methods Added License and doc

--- src/Mo/Helper/Binary.php
+++ src/Mo/Helper/Binary.php
@@ -1,15 +1,33 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2015 Maurice Prosper <maurice.prosper@ttu.edu>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 namespace Mo\Helper;
 
 /**
- * Description of Binary
+ * Layer of abstraction for binary data
  *
  * @author Maurice Prosper <maurice.prosper@ttu.edu>
  */
@@ -29,7 +47,7 @@ class Binary {
 	public function __construct($data = null, $base = 16) {
 		if($base !== 16)
 			$data = base_convert ($data, $base, 16);
-		
+
 		$this->data = pack('H*', $data);
 	}
 	
@@ -37,11 +55,51 @@ class Binary {
 	 * HEX representation of data
 	 * @return string
 	 */
-	public function toHex() {
-		$a = unpack('H*', $this->data);
-		return $a[0];
+	public function toHex($prefix = false) {
+		return ($prefix ? '0x' : null) . unpack('H*hex', $this->data)['hex'];
 	}
 	
+	/**
+	 * Binary data to char list
+	 * @return string
+	 */
+	public function toAscii() {
+		return hex2bin($this->toHex());
+	}
+	
+	/**
+	 * Binary data to Binary
+	 * @return string
+	 */
+	public function toBinary() {
+		$ret = '';
+		foreach(str_split($this->toHex(), 1) as $char)
+			$ret .= str_pad(base_convert ($char, 16, 2), 4, '0', STR_PAD_LEFT);
+		
+		return $ret;
+	}
+	
+	/**
+	 * Binary data to Base 10
+	 * Not safe for calculations
+	 * 
+	 * @return float|int
+	 */
+	public function toDecimal() {
+		return hexdec($this->toHex());
+	}
+	
+	/**
+	 * Unpacks the data
+	 * 
+	 * @param string $format
+	 * @return array
+	 */
+	public function unpack($format) {
+		return unpack($format, $this->data);
+	}
+
+
 	/**
 	 * Creates a random binary patter
 	 * @param int $bits number of bits, rounds to lowest nibble
@@ -56,6 +114,6 @@ class Binary {
 		}
 		$data = substr($data, 0, $bits);
 
-		return new static($data, 2);
+		return new static(bin2hex($data), 16);
 	}
 }
